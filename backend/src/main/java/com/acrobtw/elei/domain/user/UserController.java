fc59e9100ef903668a2f5ab073ca21106c4e45cd
nn1221nn <acrobtw@gmail.com>
2026-05-30 23:40:52 +0300

    refactor: now controller have no idea about repository feat: dailyCheckIn PostMapping request

--- backend/src/main/java/com/acrobtw/elei/domain/user/UserController.java
+++ backend/src/main/java/com/acrobtw/elei/domain/user/UserController.java
@@ -4,10 +4,10 @@ import java.security.Principal;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.acrobtw.elei.core.exception.ResourceNotFoundException;
 import com.acrobtw.elei.domain.user.dto.UserProfileResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,14 +19,20 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/users")
 @Tag(name = "User", description = "User profile operations")
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
+    private final UserStreakService userStreakService;
+
+
 
     @Operation(summary = "Get user profile", description = "Returns basic profile data for the currently authenticated user")
     @GetMapping("/profile")
     public ResponseEntity<UserProfileResponse> getProfile(Principal principal) {
-        String username = principal.getName();
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException("User not found: " + username));
+        return ResponseEntity.ok(userService.getUserProfile(principal.getName()));
+    }
 
-        return ResponseEntity.ok(new UserProfileResponse(user.getUsername(), user.getEmail()));
+    @Operation(summary = "Activate daily protocol", description = "Updates user streak and awards daily bonus XP")
+    @PostMapping("check-in")
+    public ResponseEntity<String> dailyCheckIn(Principal principal) {
+        return ResponseEntity.ok(userStreakService.processDailyCheckIn(principal.getName()));
     }
 }
