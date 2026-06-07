8c23b1050669126474eeb1b849032535fb206e3f
nn1221nn <acrobtw@gmail.com>
2026-06-07 03:05:37 +0300

    feat(quest): add repository method(findByIsDefaultTrue) to fetch only default quests

--- backend/src/main/java/com/acrobtw/elei/domain/quest/repository/QuestRepository.java
+++ backend/src/main/java/com/acrobtw/elei/domain/quest/repository/QuestRepository.java
@@ -1,9 +1,12 @@
 package com.acrobtw.elei.domain.quest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.acrobtw.elei.domain.quest.Quest;
 
 public interface QuestRepository extends JpaRepository<Quest, Long> {
 
+    List<Quest> findByIsDefaultTrue();
 }
