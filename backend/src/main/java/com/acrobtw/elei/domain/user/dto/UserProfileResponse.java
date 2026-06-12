03cd4313b7bbc94b01964f36def3869de550eae0
nn1221nn <56519459+nn1221nn@users.noreply.github.com>
2026-06-12 08:54:07 +0300

    feat(user): add new fields for better class DTO identity

--- backend/src/main/java/com/acrobtw/elei/domain/user/dto/UserProfileResponse.java
+++ backend/src/main/java/com/acrobtw/elei/domain/user/dto/UserProfileResponse.java
@@ -8,6 +8,7 @@ import com.acrobtw.elei.domain.auth.enums.AuthProvider;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Basic profile information of the user")
 public record UserProfileResponse(
@@ -25,6 +26,22 @@ public record UserProfileResponse(
     @NotBlank
     AuthProvider provider,
 
-    @Schema(description = "List of dates the user was active in YYY-MM-DD format", example = "[\"2026-05-30\", \"2026-05-31\"]")
-    List<String> activeDays
+    @Schema(description = "List of dates the user was active in YYY-MM-DD format", example = "[\"2026-05-30\", \"2026-05-31\"]", requiredMode =  Schema.RequiredMode.REQUIRED)
+    List<String> activeDays,
+
+    @Schema(description = "User's current level", example = "2", requiredMode =  Schema.RequiredMode.REQUIRED)
+    @NotNull
+    Integer level,
+
+    @Schema(description = "Total accumulated experience", example = "150", requiredMode =  Schema.RequiredMode.REQUIRED)
+    @NotNull
+    Long totalExperience,
+
+    @Schema(description = "Experience required for the next level", example = "300", requiredMode =  Schema.RequiredMode.REQUIRED)
+    @NotNull
+    Long nextLevelExperience,
+
+    @Schema(description = "Citizen rank title", example = "Novice", requiredMode =  Schema.RequiredMode.REQUIRED)
+    @NotBlank
+    String rank
 ) {}
